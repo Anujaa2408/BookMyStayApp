88e1a99bc84f948626ca7dae99b5f7b16a3f2c5b
Anujaa C <anujaac0624@gmail.com>
2026-04-09 08:39:32 +0530

    UC1 - Application Entry & Welcome Message

---
+++ src/main.java
@@ -0,0 +1,29 @@
+/**
+ * Hotel Booking Management System
+ * Entry point of the application
+ * Displays welcome message and system information
+ *
+ * @author Anujaa
+ * @version 1.0
+ */
+public class main {
+
+    /**
+     * Main method - starting point of the program
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=================================");
+        System.out.println("   Welcome to BookMyStay App   ");
+        System.out.println("=================================");
+
+        // Application details
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+        // Exit message
+        System.out.println("Thank you for using the application!");
+    }
+}
